3350ffd62440c4f1bf5405058cf4f42e680c01fd
Reece Gibson <susifrozee@gmail.com>
2021-05-22 16:06:07 +0100

    First Commit!
    Start of the project.

---
+++ Gonzo3d/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gonzo3d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
